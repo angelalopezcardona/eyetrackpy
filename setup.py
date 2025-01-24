343d69e83ec022c708b798b0c17706377fcfe2b4
anlopez94 <angelalopezcardona94@gmail.com>
2025-01-24 15:29:40 +0100

    .

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ setup(
     name="eyetrackpy",
     version="0.1",
     packages=find_packages(),
-    install_requires=requirements,
+     install_requires=[
+        # List your dependencies here
+        'numpy',
+        'pandas',
+        # Add other dependencies as needed
+    ],
     # other metadata like author, description, license, etc.
+    author="Angela Lopez",
+    description="A Python package for eye tracking analysis",
 )
